eda7242bd08f8c24e1781934b5b535565ea9d679
taokong <taokongcn@gmail.com>
2020-04-02 08:09:24 +0800

    Revert "add 'find_unused_parameters' in dist train"
    This reverts commit 10830bc7b270225bb947b138db54fd4b2580a2f9.

--- tools/test_robustness.py
+++ tools/test_robustness.py
@@ -375,11 +375,7 @@ def main():
                 model = MMDataParallel(model, device_ids=[0])
                 outputs = single_gpu_test(model, data_loader, args.show)
             else:
-                # model = MMDistributedDataParallel(model.cuda())
-                model = MMDistributedDataParallel(
-                    model.cuda(),
-                    device_ids=[torch.cuda.current_device()],
-                    broadcast_buffers=False)
+                model = MMDistributedDataParallel(model.cuda())
                 outputs = multi_gpu_test(model, data_loader, args.tmpdir)
 
             rank, _ = get_dist_info()
