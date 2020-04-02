7b4cc7bf5fe1b04a4e00f057b3384fe7ec01408a
taokong <taokongcn@gmail.com>
2020-04-02 08:13:16 +0800

    using mmcv==0.2.16

--- mmdet/apis/train.py
+++ mmdet/apis/train.py
@@ -205,15 +205,7 @@ def _dist_train(model,
         for ds in dataset
     ]
     # put model on gpus
-    # model = MMDistributedDataParallel(model.cuda())
-    find_unused_parameters = True
-    # Sets the `find_unused_parameters` parameter in
-    # torch.nn.parallel.DistributedDataParallel
-    model = MMDistributedDataParallel(
-        model.cuda(),
-        device_ids=[torch.cuda.current_device()],
-        broadcast_buffers=False,
-        find_unused_parameters=find_unused_parameters)
+    model = MMDistributedDataParallel(model.cuda())
 
     # build runner
     optimizer = build_optimizer(model, cfg.optimizer)
